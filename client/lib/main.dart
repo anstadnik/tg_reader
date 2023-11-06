0fd10caea8e733dd247b8bd6c57ac6f28776527c
Stadnik Andrii <stadnikandriy1@gmail.com>
2023-11-06 09:34:37 +0200

    Working playing messages in background.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,56 @@
+import 'dart:async';
+
+import 'package:client/my_ja_bytes_source.dart';
 import 'package:client/myHomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:workmanager/workmanager.dart';
 
-void main() {
+@pragma('vm:entry-point')
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    print('Task: $task');
+    print('Data: $inputData');
+
+    Completer<bool> completer = Completer<bool>();
+    WebSocketChannel channel = WebSocketChannel.connect(Uri.parse(task));
+    final player = AudioPlayer();
+
+    channel.stream.listen(
+      (dynamic bytes) async {
+        try {
+          await player.setAudioSource(MyJABytesSource(bytes));
+          player.processingStateStream.listen((ProcessingState state) {
+            if (state == ProcessingState.ready) {
+              // player.pause();
+              print('ready');
+              player.play();
+            }
+          });
+        } catch (e) {
+          // Handle exception when setting the audio source
+          completer.complete(false);
+        }
+      },
+      // When the WebSocket is done, complete the completer with true.
+      onDone: () => completer.complete(true),
+      // If there's an error, complete the completer with false.
+      onError: (error) => completer.complete(false),
+      // Passing a cancelOnError as true means the subscription is automatically canceled
+      // when the first error event is delivered.
+      cancelOnError: true,
+    );
+
+    // The `executeTask` must return a future.
+    // The completer's future will complete when the WebSocket is done or an error occurs.
+    return completer.future;
+  });
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
   runApp(const MyApp());
 }
 
@@ -14,21 +63,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TG Listener',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
