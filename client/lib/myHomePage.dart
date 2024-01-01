e6614ef8c79b4848c8b0d21d68e4eb6216b01446
Stadnik Andrii <stadnikandriy1@gmail.com>
2024-01-01 20:30:16 +0200

    Update client

--- client/lib/myHomePage.dart
+++ client/lib/myHomePage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:workmanager/workmanager.dart';
-import 'package:http/http.dart' as http;
+import 'package:client/url_connect.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -13,29 +12,8 @@ const String emulatorUrl = "ws://10.0.2.2:8000/ws";
 const String localNetUrl = "ws://192.168.31.44:8000/ws";
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _url = emulatorUrl;
-  int _taskId = 0;
-  bool _urlPings = false;
-
-  Future<void> connectWebSocket() async {
-    Workmanager().cancelAll();
-
-    final response =
-        await http.head(Uri.parse(_url).replace(scheme: "http", path: "docs"));
-    print(response.statusCode);
-    if (response.statusCode == 200) {
-      setState(() => _urlPings = true);
-      Workmanager().registerOneOffTask((_taskId++).toString(), _url);
-    } else {
-      setState(() => _urlPings = false);
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    connectWebSocket();
-  }
+  int currentPageIndex = 0;
+  final PageController _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
@@ -44,53 +22,47 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Telegram listener"),
       ),
-      body: Column(
-        children: [
-          RichText(
-            text: TextSpan(
-              style: Theme.of(context).textTheme.bodyText1,
-              children: [
-                TextSpan(
-                    text: _urlPings ? "URL is pinging" : "URL is not pinging"),
-                WidgetSpan(
-                  child: Icon(
-                    _urlPings ? Icons.check_circle : Icons.cancel,
-                    color: _urlPings ? Colors.green : Colors.red,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: TextEditingController(text: _url),
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter URL',
-              ),
-              onSubmitted: (value) => setState(() => _url = value),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ActionChip(
-                label: const Text("Emulator URL"),
-                onPressed: () => setState(() => _url = emulatorUrl),
-              ),
-              ActionChip(
-                label: const Text("Local net example URL"),
-                onPressed: () => setState(() => _url = localNetUrl),
-              ),
-            ],
-          ),
+      bottomNavigationBar: NavigationBar(
+        animationDuration: const Duration(milliseconds: 2000),
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          setState(() => currentPageIndex = index);
+          _pageController.animateToPage(index,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut);
+        },
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.link), label: 'Connect'),
+          NavigationDestination(
+              icon: Icon(Icons.settings), label: 'Change server handlers'),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () => connectWebSocket(),
-          tooltip: 'Connect',
-          child: const Icon(Icons.link)),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) => setState(() => currentPageIndex = index),
+        children: const [URLConnect(), ServerHandlers()],
+      ),
     );
+  }
+}
+
+class ServerHandlers extends StatefulWidget {
+  const ServerHandlers({super.key});
+
+  @override
+  State<ServerHandlers> createState() => _ServerHandlersState();
+}
+
+class _ServerHandlersState extends State<ServerHandlers> {
+  List<String> handlers = [];
+
+  Future<void> loadHandlers() async {
+    handlers = await getHandlers();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("WTF");
   }
 }
